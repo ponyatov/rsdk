37b1e0cc0d5dbcebdece528c124b086a76a267e3
Dmitry Ponyatov <dponyatov@gmail.com>
2026-01-14 16:56:43 +0400

    .

--- mk/rust.mk
+++ mk/rust.mk
@@ -1,9 +1,6 @@
 RTARGET = x86_64-unknown-linux-gnu
 
 $(RUSTUP) $(CARGO):
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	rustup target add x86_64-unknown-linux-gnu
-$(RUSTUP) $(CARGO):
 # PROXY = -x 10.110.1.12:8888
 	curl $(PROXY) --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 	. $HOME/.cargo/env
